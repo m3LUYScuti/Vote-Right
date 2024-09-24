d051c9e246b57daad4bfe0c1fa98503dfa2edf94
davit <davit@GOSH>
2024-09-24 17:28:53 +0400

    Add project files.

---
+++ Over 18/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Sheiyvanet tqveni asaki");
+double Asaki = double.Parse(Console.ReadLine());
+
+if (Asaki >= 18)
+{
+    Console.WriteLine("Dashvebuli xart archevnebze");
+}
+else if (Asaki < 18)
+{
+    Console.WriteLine("Tqven xart arasrulwlovani da ar daishvebit archevnebze");
+}
+else
+{
+    Console.WriteLine("gtxovt sheiyvanot swori monacemi");
+}
+
+
+Console.ReadLine();
